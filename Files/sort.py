1566914b0cc5d8f7e2aacf38875f06bf8e1eb4cf
SublimateTheBerry <83465249+SublimateTheBerry@users.noreply.github.com>
2025-08-15 15:17:28 +0300

    Create sort.py

---
+++ Files/sort.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import base64
+
+
+ptt = os.path.abspath(os.path.join(os.getcwd(), '..'))
+vmess_file = os.path.join(ptt, 'Splitted-By-Protocol/vmess.txt')
+vless_file = os.path.join(ptt, 'Splitted-By-Protocol/vless.txt')
+trojan_file = os.path.join(ptt, 'Splitted-By-Protocol/trojan.txt')
+ss_file = os.path.join(ptt, 'Splitted-By-Protocol/ss.txt')
+ssr_file = os.path.join(ptt, 'Splitted-By-Protocol/ssr.txt')
+
+os.makedirs(os.path.dirname(vmess_file), exist_ok=True)
+
+open(vmess_file, "w").close()
+open(vless_file, "w").close()
+open(trojan_file, "w").close()
+open(ss_file, "w").close()
+open(ssr_file, "w").close()
+
+vless = ""
+trojan = ""
+ss = ""
+ssr = ""
+config_path = os.path.join(ptt, 'All_Configs_Sub.txt')
+with open(config_path, 'r', encoding='utf-8') as f:
+    respnse = f.read()
+for config in respnse.splitlines():
+    if config.startswith("vmess"):
+        open(vmess_file, "a").write(config + "\n")     
+    if config.startswith("vless"):
+        vless += config + "\n"  
+    if config.startswith("trojan"):
+        trojan += config + "\n"   
+    if config.startswith("ss"):   
+        ss += config + "\n"
+    if config.startswith("ssr"):
+        ssr += config + "\n"
+ 
+open(vless_file, "w").write(base64.b64encode(vless.encode("utf-8")).decode("utf-8"))  
+open(trojan_file, "w").write(base64.b64encode(trojan.encode("utf-8")).decode("utf-8"))  
+open(ss_file, "w").write(base64.b64encode(ss.encode("utf-8")).decode("utf-8"))  
+open(ssr_file, "w").write(base64.b64encode(ssr.encode("utf-8")).decode("utf-8"))  
